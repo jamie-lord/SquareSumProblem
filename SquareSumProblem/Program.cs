b0a4f9d346343afcf4ea00ffca7f03d7c74554ba
Jamie Lord <jamie@lord.technology>
2018-01-13 15:49:03 +0000

    Finds all permulations of array

---
+++ SquareSumProblem/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace SquareSumProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var array = CreateStaringArray(15);
+
+            Permutations(array, 0, array.Length - 1);
+        }
+
+        private static int[] CreateStaringArray(int length)
+        {
+            return Enumerable.Range(1, length).ToArray();
+        }
+
+        private static void SwapTwoNumbers(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        private static void Permutations(int[] list, int k, int m)
+        {
+            int i;
+            if (k == m)
+            {
+                for (i = 0; i <= m; i++)
+                {
+                    Console.Write($"{list[i]} ");
+                }
+                Console.Write("\n");
+            }
+            else
+                for (i = k; i <= m; i++)
+                {
+                    SwapTwoNumbers(ref list[k], ref list[i]);
+                    Permutations(list, k + 1, m);
+                    SwapTwoNumbers(ref list[k], ref list[i]);
+                }
+        }
+    }
+}
